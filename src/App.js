76e01b9e92e04a6414adee2bc7e6eed889cbe4f6
Jay <160652025+codelover1226@users.noreply.github.com>
2024-06-11 12:35:56 +0900

    update work

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ export default function App() {
         <div className="w-screen bg-[#F4F5F7] absolute " style={{ overscrollBehavior: 'contain' }}>
           <div className="max-w-[1600px] mx-auto md:rounded-2xl bg-white h-[100svh] inset-0 shadow-xl  overflow-x-hidden overflow-y-hidden relative">
               <Routes>
-                {/* <Route path="/buy" exact element= {<Landingpage />}/>
+                <Route path="/buy" exact element= {<Landingpage />}/>
                 <Route path="/sell" exact element= {<Landingpage />}/>
                 <Route path="/rent" exact element= {<Landingpage />}/>
-                <Route path="/" exact element= {<Landingpage />}/> */}
+                <Route path="/" exact element= {<Landingpage />}/>
               </Routes>
           </div>
         </div>       
